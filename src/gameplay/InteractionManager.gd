9aabda80c69868e594b6519367e2d120a9e2a8e4
Gusse <gustafssonmik4el@gmail.com>
2024-10-14 22:57:44 +0300

    Improve interaction

--- src/gameplay/InteractionManager.gd
+++ src/gameplay/InteractionManager.gd
@@ -1,5 +1,8 @@
 extends Node
 
+@onready var cursor_ui = $"../UserInterface/StrideIndicatorContainer"
+@onready var cd_case_image = $"../UserInterface/InGameUI/Items/CDCaseImage"
+
 var looked_at_area: Area3D
 var can_interact: bool
 
@@ -8,10 +11,15 @@ func _process(delta: float) -> void:
 			print_debug("Interacting...")
 			if looked_at_area:
 				looked_at_area.interact()
-
+				if looked_at_area.name == "CD_Case":
+					_on_cd_case_picked_up()
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	looked_at_area = area
 
 func _on_area_3d_area_exited(area: Area3D) -> void:
 	looked_at_area = null
+
+func _on_cd_case_picked_up() -> void:
+	print_debug("Registered CD_Case pickup")
+	cd_case_image.visible = true
--- src/gameplay/events/CDCase.gd
+++ src/gameplay/events/CDCase.gd
@@ -3,10 +3,15 @@ extends "res://src/gameplay/Event.gd"
 @export var original_situation: Node3D
 @export var new_situation: Node3D
 
+signal cd_case_picked_up
+
 func interact():
 	print_debug("Interacted with CD Case")
 	original_situation.process_mode = Node.PROCESS_MODE_DISABLED
 	original_situation.visible = false
 
-	new_situation.process_mode = Node.PROCESS_MODE_ALWAYS
+	new_situation.process_mode = Node.PROCESS_MODE_INHERIT
 	new_situation.visible = true
+
+	get_parent().visible = false
+	get_parent().process_mode = Node.PROCESS_MODE_DISABLED
