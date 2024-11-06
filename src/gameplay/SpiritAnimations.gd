4db4fedbca7f585a254f1f841f51644c29864c19
Gusse <gustafssonmik4el@gmail.com>
2024-11-07 00:17:36 +0200

    Begin map 2

--- src/gameplay/SpiritAnimations.gd
+++ src/gameplay/SpiritAnimations.gd
@@ -3,8 +3,8 @@ extends AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	play("spirit_idle")
-
+	#play("spirit_idle")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
